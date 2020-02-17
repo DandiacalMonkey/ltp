7f06e1cf9f2a313c0e740264e3343ea289ca3822
hgl0367 <60907256+hgl0367@users.noreply.github.com>
2020-02-17 16:37:56 +0800

    hua.g.l:加工界面细节修改

--- base/loadbar.cpp
+++ base/loadbar.cpp
@@ -6,8 +6,8 @@ using ltp::base::LoadBar;
 LoadBar::LoadBar(QWidget *parent)
 	: QProgressBar(parent)
 {
-	this->setStyleSheet("QProgressBar{text-align:right; margin-right:35px; margin-top:3px; margin-bottom:3px; font:14px Arial;}");
-	this->setFixedSize(145, 14);
+	this->setStyleSheet("QProgressBar{text-align:right; margin-right:40px; margin-top:3px; margin-bottom:3px; font:14px Arial;}");
+	this->setFixedSize(146, 14);
 
 	connect(this, SIGNAL(valueChanged(int)), this, SLOT(colorChanged(int)));
 }
@@ -20,17 +20,17 @@ void LoadBar::colorChanged(int value)
 {
 	if (value < 40)			// 绿色范围
 	{
-		this->setStyleSheet("QProgressBar{text-align:right; margin-right:35px; margin-top:3px; margin-bottom:3px; font:14px Arial;} \
+		this->setStyleSheet("QProgressBar{text-align:right; margin-right:40px; margin-top:3px; margin-bottom:3px; font:14px Arial;} \
 							 QProgressBar::chunk { background-color: #00AA3C }");
 	} 
 	else if (value >= 40 && value < 70)			// 黄色范围
 	{
-		this->setStyleSheet("QProgressBar{text-align:right; margin-right:35px; margin-top:3px; margin-bottom:3px; font:14px Arial;} \
+		this->setStyleSheet("QProgressBar{text-align:right; margin-right:40px; margin-top:3px; margin-bottom:3px; font:14px Arial;} \
 							 QProgressBar::chunk { background-color: #FFD524 }");
 	}
 	else				// 红色警示范围
 	{
-		this->setStyleSheet("QProgressBar{text-align:right; margin-right:35px; margin-top:3px; margin-bottom:3px; font:14px Arial;} \
+		this->setStyleSheet("QProgressBar{text-align:right; margin-right:40px; margin-top:3px; margin-bottom:3px; font:14px Arial;} \
 							 QProgressBar::chunk { background-color: #FA3200 }");
 	}
 }
